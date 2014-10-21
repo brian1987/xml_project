3ad5fcb92955046be46c967f37c7e54bb5ec2ee5
brian1987 <pisano.b@gmail.com>
2014-10-21 11:21:20 -0400

    xml stuff

--- app/models/xml_builder.rb
+++ app/models/xml_builder.rb
@@ -1,6 +1,13 @@
 class XmlBuilder < ActiveRecord::Base
-  require 'rexml/document'
-  require 
-  doc = REXML::Document.new
+  # Ruby Cookbook Solution
+  # require 'rexml/document'
+  # require 
+  # doc = REXML::Document.new
+
+  # Builder Gem solution
+
+  builder = Builder::XmlMarkup.new(:target=>STDOUT, :indent=>2)
+  xml = builder.person { |b| b.name("Brian"); b.phone("441-8885")}
+  # xml
 
 end
